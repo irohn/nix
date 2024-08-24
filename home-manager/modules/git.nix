10153277c69178350610fa48436791c4dda7efd6
iro <orisneh@gmail.com>
2024-08-24 18:12:12 +0300

    Added git aliases

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -5,5 +5,20 @@
     enable = true;
     userName = defaults.username;
     userEmail = defaults.email;
+    aliases = {
+      unstage = "reset HEAD --";
+      sync = "!f() { \
+        local branch=$(git rev-parse --abbrev-ref HEAD); \
+        git fetch origin $branch:$branch; \
+        git merge-base --is-ancestor $branch origin/$branch || \
+          (git stash && git rebase origin/$branch && git stash pop); \
+        git push origin $branch; \
+      }; f";
+    };
+    extraConfig = {
+      pull = {
+        rebase = true;
+      };
+    };
   };
 }
