9bb7222fd992808233e8f3557a6fedc9780f0b7a
iro <orisneh@gmail.com>
2024-08-25 00:13:36 +0300

    Adjusted tmux config

--- home-manager/modules/tmux.nix
+++ home-manager/modules/tmux.nix
@@ -5,19 +5,77 @@
 {
   programs.tmux = {
     enable = true;
-    terminal = "tmux-256color";
     extraConfig = ''
-      set -g prefix2 C-Space
-      bind C-Space send-prefix
-      set -g escape-time 0
-      set -g history-limit 100000
+      # Address vim mode switching delay (http://superuser.com/a/252717/65504)
+      set -s escape-time 0
+
+      # Increase scrollback buffer size from 2000 to 50000 lines
+      # set history file path
+      set -g history-limit 50000
       set -g history-file ~/.config/tmux/log/tmuxhistory
+
+      # Increase tmux messages display duration from 750ms to 4s
+      set -g display-time 4000
+
+      # Refresh 'status-left' and 'status-right' more often, from every 15s to 5s
+      set -g status-interval 5
+
+      # Upgrade $TERM
+      set -g default-terminal "screen-256color"
+
+      # Emacs key bindings in tmux command prompt (prefix + :) are better than
+      # vi keys, even for vim users
+      set -g status-keys emacs
+
+      # Focus events enabled for terminals that support them
+      set -g focus-events on
+
+      # Super useful when using "grouped sessions" and multi-monitor setup
+      setw -g aggressive-resize on
+
+      # Start from index 1 for easier tab switching
+      set -g base-index 1
+      set -g renumber-windows on
+
+      # Enable mouse
+      set -g mouse on
+
+      # Easier and faster switching between next/prev window
+      bind C-p previous-window
+      bind C-n next-window
+
+      # Prefix
+      unbind C-b
+      set-option -g prefix C-space
+      bind C-Space send-prefix
+      bind space last-window
+
+      # Sync system clipboard with tmux's
+      set -g set-clipboard external
+
+      # Prevent tmux from displaying "Activity in window n"
       set -g monitor-activity off
       set -g visual-activity off
+
+      # remove bell alerts
       setw -g monitor-bell off
       set -g bell-action none
-      set -g set-clipboard on
-      setw -g wrap-search off
+
+      # Source tmux config
+      bind r source-file ~/.config/tmux/tmux.conf
+
+      # Pane navigation
+      bind h select-pane -L
+      bind l select-pane -R
+      bind k select-pane -U
+      bind j select-pane -D
+
+      # Pane borders
+      set -g pane-border-style 'fg=#DCD7BA'
+      set -g pane-active-border-style 'fg=#6A9589'
+      set -g pane-border-lines "double"
+
+      # Status bar colors and configuration
       set -g status-position top
       set -g status on
       set -g status-interval 1
@@ -30,10 +88,7 @@
       set -g status-justify centre
       set -g status-right-length 40
       set -g status-right "#[fg=#7E9CD8,bg=#16161D] %H:%M:%S"
-      set -g base-index 1
-      set -g renumber-windows on
       set -g message-style "fg=#1F1F28,bg=#E6C384"
-      set -g mouse on
     '';
   };
 }
