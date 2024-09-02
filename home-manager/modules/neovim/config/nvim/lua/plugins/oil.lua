477a07ad81b6f4bf5a6cb61ebf7d46c95570bf78
iro <orisneh@gmail.com>
2024-09-02 18:08:41 +0300

    added git and details to oil

--- home-manager/modules/neovim/config/nvim/lua/plugins/oil.lua
+++ home-manager/modules/neovim/config/nvim/lua/plugins/oil.lua
@@ -6,20 +6,101 @@ return {
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
+    local detail = false
+
+    -- helper function to parse output
+    local function parse_output(proc)
+      local result = proc:wait()
+      local ret = {}
+      if result.code == 0 then
+        for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+          -- Remove trailing slash
+          line = line:gsub("/$", "")
+          ret[line] = true
+        end
+      end
+      return ret
+    end
+
+    -- build git status cache
+    local function new_git_status()
+      return setmetatable({}, {
+        __index = function(self, key)
+          local ignore_proc = vim.system(
+            { "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" },
+            {
+              cwd = key,
+              text = true,
+            }
+          )
+          local tracked_proc = vim.system({ "git", "ls-tree", "HEAD", "--name-only" }, {
+            cwd = key,
+            text = true,
+          })
+          local ret = {
+            ignored = parse_output(ignore_proc),
+            tracked = parse_output(tracked_proc),
+          }
+
+          rawset(self, key, ret)
+          return ret
+        end,
+      })
+    end
+    local git_status = new_git_status()
+
+    -- Clear git status cache on refresh
+    local refresh = require("oil.actions").refresh
+    local orig_refresh = refresh.callback
+    refresh.callback = function(...)
+      git_status = new_git_status()
+      orig_refresh(...)
+    end
+
     require("oil").setup({
       win_options = {
         wrap = false,
         signcolumn = "no",
-        cursorcolumn = true,
+        cursorcolumn = false,
         foldcolumn = "0",
         spell = false,
         list = false,
         conceallevel = 3,
         concealcursor = "nvic",
+        number = false,
+        relativenumber = false,
+      },
+      keymaps = {
+        ["<leader>od"] = {
+          desc = "Toggle file detail view",
+          callback = function()
+            detail = not detail
+            if detail then
+              require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+            else
+              require("oil").set_columns({ "icon" })
+            end
+          end,
+        },
+      },
+      view_options = {
+        is_hidden_file = function(name, bufnr)
+          local dir = require("oil").get_current_dir(bufnr)
+          local is_dotfile = vim.startswith(name, ".") and name ~= ".."
+          -- if no local directory (e.g. for ssh connections), just hide dotfiles
+          if not dir then return is_dotfile end
+          -- dotfiles are considered hidden unless tracked
+          if is_dotfile then
+            return not git_status[dir].tracked[name]
+          else
+            -- Check if file is gitignored
+            return git_status[dir].ignored[name]
+          end
+        end,
       },
     })
 
-    vim.keymap.set("n", "<leader>e", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+    vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
   end,
 }
