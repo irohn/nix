a8eb0435eff727639f4fb61f868894ddc25bbb6d
iro <orisneh@gmail.com>
2024-09-04 02:57:03 +0300

    Added github key to systems

--- secrets.nix
+++ secrets.nix
@@ -1,12 +1,13 @@
 let
   ori = [
-    # public keys
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPjGVE8CXIupUSSzv7gX25FOM7u6AMWG63LeJKX2EmX6" # irohn
+    # public keys (user level ~/.ssh)
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICREe77Tw/a2lXSDgMi6sS/a4U3m/CamEsyVEheIu8FO" # macbook
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFRgZJvd679bdOO+9W5umSX4JxLzCsztZvzuREOfc/E9" # wsl-ubuntu
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKmdDp+AczoOuFgt2NM54EEIYa7U3ZNgjHWCe19J5n6p" # pinix
   ];
   systems = [
+    # public keys (system level /etc/ssh)
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPjGVE8CXIupUSSzv7gX25FOM7u6AMWG63LeJKX2EmX6" # github (1password)
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPdFUkUYAgMzaGpSDRa35Dm8Ti+i8wX9Utbyk6yiB+w3" # macbook pro
   ];
 
