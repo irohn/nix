95ab14bd5bf60cff969ed5361dd9a3fa944759d5
nixos <orisneh@gmail.com>
2024-09-07 14:16:13 +0000

    Replaced mini.files with oil.nvim

--- home-manager/modules/neovim/config/nvim/lua/plugins/explorer.lua
+++ home-manager/modules/neovim/config/nvim/lua/plugins/explorer.lua
@@ -1,14 +1,58 @@
 return {
   {
-    'echasnovski/mini.nvim',
-    version = false,
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} }, { "nvim-lua/plenary.nvim" }, },
+    lazy = false,
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
     config = function()
-      require('mini.files').setup({
-        windows = {
-          preview = true,
+      local oil = require("oil")
+      oil.setup({
+        default_file_explorer = true,
+        win_options = {
+          wrap = false,
+          signcolumn = "no",
+          cursorcolumn = false,
+          foldcolumn = "0",
+          spell = false,
+          list = false,
+          conceallevel = 3,
+          concealcursor = "nvic",
+          number = false,
+          relativenumber = false,
+        },
+        float = {
+          -- Padding around the floating window
+          padding = 2,
+          max_width = 0,
+          max_height = 0,
+          border = "rounded",
+          win_options = { winblend = 0, },
+          -- preview_split: Split direction: "auto", "left", "right", "above", "below".
+          preview_split = "auto",
+          -- This is the config that will be passed to nvim_open_win.
+          -- Change values here to customize the layout
+          override = function(conf)
+            return conf
+          end,
         },
       })
-      vim.keymap.set("n", "<leader>e", function() MiniFiles.open() end, { desc = "Open file explorer" })
-    end,
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "OilEnter",
+        callback = vim.schedule_wrap(function(args)
+          if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
+            oil.open_preview()
+          end
+        end),
+      })
+
+      vim.keymap.set("n", "<leader>e", function()
+        oil.open_float()
+      end, { desc = "Open file explorer with preview" })
+    end
   },
 }
