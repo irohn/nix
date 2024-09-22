139479041059ed763e73f415ad2efc0de98f6715
Ori Sne <orisne@greeneye.ag>
2024-09-22 15:58:39 +0300

    added keymaps for cmp

--- home-manager/modules/neovim/config/nvim/lua/plugins/cmp.lua
+++ home-manager/modules/neovim/config/nvim/lua/plugins/cmp.lua
@@ -6,5 +6,17 @@ return {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-path",
     },
+    config = function()
+      local cmp = require 'cmp'
+      cmp.setup({
+        completion = { completeopt = 'menu,menuone,noinsert' },
+        mapping = cmp.mapping.preset.insert {
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<C-x>'] = cmp.mapping.complete {},
+        }
+      })
+    end
   },
 }
