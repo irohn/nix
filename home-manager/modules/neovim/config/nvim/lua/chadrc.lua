e6bd932e7ee8f9d9bde43d53526276a85e41dcf3
nixos <orisneh@gmail.com>
2024-10-02 23:31:11 +0000

    Added nvdash

--- home-manager/modules/neovim/config/nvim/lua/chadrc.lua
+++ home-manager/modules/neovim/config/nvim/lua/chadrc.lua
@@ -10,5 +10,29 @@ M.ui = {
   },
 }
 
-return M
+M.nvdash = {
+  load_on_startup = true,
+  header = {
+    "                            ",
+    "     Powered By  eovim    ",
+    "                            ",
+  },
+  buttons = {
+    { txt = "  Find File", keys = "Spc f f", cmd = "Telescope find_files" },
+    { txt = "  Recent Files", keys = "Spc f o", cmd = "Telescope oldfiles" },
+    { txt = "󰈭  Find Word", keys = "Spc f g", cmd = "Telescope live_grep" },
+    { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
+    {
+      txt = function()
+	local stats = require("lazy").stats()
+	local ms = math.floor(stats.startuptime) .. " ms"
+	return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+      end,
+      hl = "NvDashLazy",
+      no_gap = true,
+    },
+    { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
+  },
+}
 
+return M
