e6240cd7aee90db932122eb2ea2906eaf5dd7317
Ori Sne <orisne@greeneye.ag>
2024-10-27 05:31:00 +0800

    Added transparency to nvim and wezterm

--- home-manager/modules/neovim/config/nvim/lua/plugins/themes.lua
+++ home-manager/modules/neovim/config/nvim/lua/plugins/themes.lua
@@ -24,4 +24,9 @@ return {
 		end
 	},
 
+  {
+    "xiyaowong/transparent.nvim",
+    opts = {},
+  },
+
 }
--- home-manager/modules/wezterm/config/wezterm/appearance.lua
+++ home-manager/modules/wezterm/config/wezterm/appearance.lua
@@ -3,7 +3,8 @@ local M = {}
 M.apply_to_config = function(config)
   config.window_decorations = "RESIZE"
   config.window_padding = { left = 1, right = 1, top = 0, bottom = 0 }
-  config.window_background_opacity = 1.0
+  config.window_background_opacity = 0.9
+  config.macos_window_background_blur = 20
 
   config.enable_tab_bar = true
   config.use_fancy_tab_bar = false
