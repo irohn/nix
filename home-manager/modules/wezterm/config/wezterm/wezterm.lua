aaee043baccc75743ceffb6b2b97bf2fce64f09e
Ori Sne <orisne@greeneye.ag>
2024-11-02 08:47:08 +0800

    start wezterm maximized

--- home-manager/modules/wezterm/config/wezterm/wezterm.lua
+++ home-manager/modules/wezterm/config/wezterm/wezterm.lua
@@ -3,6 +3,13 @@ local platform = require 'platform'
 local mappings = require 'mappings'
 local appearance = require 'appearance'
 
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function()
+  local tab, pane, window = mux.spawn_window{}
+  window:gui_window():maximize()
+end)
+
 local config = {}
 if wezterm.config_builder then
   config = wezterm.config_builder()
