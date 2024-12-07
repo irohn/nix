9ea406edd060bb8bcb3f38ebb69c580af5b9e2d5
ori <orisneh@gmail.com>
2024-12-07 23:45:53 +0200

    updated api key names

--- modules/common/secrets/agenix-home.nix
+++ modules/common/secrets/agenix-home.nix
@@ -4,17 +4,17 @@
   age.identityPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
   age.secrets = {
     anthropics_api_key = {
-      file = ./anthropics_api_key;
+      file = ../../../secrets/anthropics_api_key;
       mode = "644";
     };
     openai_api_key = {
-      file = ./openai_api_key;
+      file = ../../../secrets/openai_api_key;
       mode = "644";
     };
   };
 
   home.sessionVariables = {
-    ANTHROPIC_API_KEY = ''
+    ANTHROPICS_API_KEY = ''
         $(${pkgs.coreutils}/bin/cat ${config.age.secrets.anthropics_api_key.path})
     '';
     OPENAI_API_KEY = ''
