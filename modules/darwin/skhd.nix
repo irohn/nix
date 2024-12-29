10cbf2cc8d6598be1247738798926487b0b8800f
Ori Sne <orisne@greeneye.ag>
2024-12-29 12:24:48 +0200

    skhd: added more terminals to ignore cmd+h

--- modules/darwin/skhd.nix
+++ modules/darwin/skhd.nix
@@ -5,7 +5,11 @@
     enable = true;
     skhdConfig = ''
       cmd - h [
-        "alacritty" : skhd -k "ctrl - b" && skhd -k "shift - h"
+        "alacritty" : skhd -k "ctrl - b" && skhd -k "p"
+        "ghostty" : skhd -k "ctrl - b" && skhd -k "p"
+        "terminal" : skhd -k "ctrl - b" && skhd -k "p"
+        "wezterm" : skhd -k "ctrl - b" && skhd -k "p"
+        "iterm2" : skhd -k "ctrl - b" && skhd -k "p"
       ]
     '';
   };
