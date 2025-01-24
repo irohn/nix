fecb4507df860022b34af388426b0b4fc6e167eb
ori <orisneh@gmail.com>
2025-01-24 19:52:30 +0200

    added podman-desktop to desktop

--- modules/nixos/containers.nix
+++ modules/nixos/containers.nix
@@ -6,10 +6,8 @@
   virtualisation = {
     podman = {
       enable = true;
-
       # Create a `docker` alias for podman, to use it as a drop-in replacement
       dockerCompat = true;
-
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
     };
@@ -19,7 +17,7 @@
   environment.systemPackages = with pkgs; [
     dive # look into docker image layers
     podman-tui # status of containers in the terminal
-    docker-compose # start group of containers for dev
-    #podman-compose # start group of containers for dev
+    podman-compose # start group of containers for dev
+    podman-desktop # desktop app
   ];
 }
