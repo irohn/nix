5d3840d4e83063cc80dae3042e59b77f24904a20
ori <orisneh@gmail.com>
2025-01-29 23:09:11 +0200

    removed addkeystoagent and forwardagent from all hosts

--- modules/home-manager/ssh/default.nix
+++ modules/home-manager/ssh/default.nix
@@ -3,14 +3,5 @@
 {
   programs.ssh = {
     enable = true;
-    addKeysToAgent = "yes";
-    forwardAgent = true;
-    matchBlocks = {
-      greeneye = {
-        hostname = "github.com";
-        user = "git";
-        identityFile = "~/.ssh/greeneye_id_ed25519";
-      };
-    };
   };
 }
