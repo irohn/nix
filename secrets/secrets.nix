08277e05713ff16510ab14cf9c21b57415a476ed
ori <orisneh@gmail.com>
2025-03-25 11:24:42 +0000

    added wsl to secret keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
   macbook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFwWiiY7dTrYDKkRE2dJfZtSgFX3IBzxt6Z0YiPPL45A";
   desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKN25wBIZ2UFyE4ygBeIlDjRX3zBy3E7O+2mldPBY9Gl";
-  users = [ macbook desktop ];
+  wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGga0B9lJdQ+TOyHZhIGuCzzqrTYq9sTelnMxcXUJJtQ";
+  users = [ macbook desktop wsl ];
 in
 {
   "anthropics_api_key".publicKeys = users;
