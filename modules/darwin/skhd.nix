25a11126c0bd4ead17361a44a9a96a9352949939
ori <orisneh@gmail.com>
2025-04-21 17:48:24 +0300

    fix skhd config

--- modules/darwin/skhd.nix
+++ modules/darwin/skhd.nix
@@ -10,6 +10,7 @@
         "terminal" : skhd -k "ctrl - b" && skhd -k "p"
         "wezterm" : skhd -k "ctrl - b" && skhd -k "p"
         "iterm2" : skhd -k "ctrl - b" && skhd -k "p"
+        "neovide" : skhd -k "ctrl - b" && skhd -k "p"
       ]
       cmd - [1-9] [
         "neovide" : skhd -k "ctrl - b" && skhd -k "$(/usr/bin/printf '%s' "''${SKHD_KEY}")"
